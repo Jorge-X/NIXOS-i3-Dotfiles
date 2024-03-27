7c5d8df0785ee147656b5ea9d4aef7b2a153d0c5
Jorge Luis <140755201+Jorge-X@users.noreply.github.com>
2024-03-27 19:18:57 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -117,6 +117,8 @@
   scrot
   nitrogen
   playerctl
+  neofetch
+  htop
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
